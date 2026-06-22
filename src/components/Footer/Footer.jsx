1b3233233af0365f9252679c4da54df4015dac82
KevinBaudson <kevinbaudson@hotmail.com>
2026-06-22 19:36:03 -0300

    adicionando endereço no footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -47,8 +47,17 @@ function Footer() {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3748.608594354249!2d-44.0304649!3d-20.0249356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6beb037742465%3A0x3fb51b7378702bc0!2sR.%20M%C3%A1rcia%20de%20Windsor%2C%2025%20-%20%C3%81guia%20Dourada%2C%20Belo%20Horizonte%20-%20MG%2C%2032405-567!5e0!3m2!1spt-BR!2sbr!4v1782165291809!5m2!1spt-BR!2sbr"
               loading="lazy"
-               className="footer-map"
+              className="footer-map"
             ></iframe>
+            <p className="footer-address">
+              <a
+                href="https://www.google.com/maps/search/?api=1&query=Rua+Márcia+de+Windsor+25+Belo+Horizonte"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Rua Márcia de Windsor, 25 - Mineirão, Belo Horizonte - MG
+              </a>
+            </p>
           </div>
         </div>
       </div>
